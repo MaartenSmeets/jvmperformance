c988b3074a4746181b637e2d2ea1c11ec41dbb60
Maarten Smeets <MaartenSmeets@users.noreply.github.com>
2018-12-15 12:50:48 +0100

    Adding function for cleaning the image

--- testscripts/runtest.sh
+++ testscripts/runtest.sh
@@ -1,32 +1,43 @@
 #!/bin/bash
 
-#first make sure spring-boot-jdk exists. for example with: docker build -t spring-boot-jdk -f Dockerfile --build-arg JAR_FILE=reactive-rest-service.jar .
-#next do docker-compose up before starting this script
-#make sure the files / directories from the below variables are reachable
+# 1 make sure spring-boot-jdk exists. for example with: docker build -t spring-boot-jdk -f Dockerfile --build-arg JAR_FILE=reactive-rest-service.jar .
+# 2 start process-exporter: docker run -d --rm -p 9256:9256 --privileged -v /proc:/host/proc -v `pwd`/proc-exp:/config ncabatoff/process-exporter --procfs /host/proc -config.path /config/process-exporter.yml 
+# for additional information on process exporter see: https://github.com/ncabatoff/process-exporter
+# 3 next do docker-compose up
+# this starts the containers for monitoring and creates the network
+# 4 make sure the files / directories from the below variables are reachable
+# 5 start this script
 
 jarfile_reactiverest=reactive-rest-service.jar
 soapui_bin=/home/developer/SmartBear/SoapUI-5.4.0/bin
 soapui_outputdir=`pwd`/jdktest
 soapui_testproject=/home/developer/REST-Project-1-soapui-project.xml
 
-function rebuild() {
+function clean_image() {
 	docker stop spring-boot-jdk
 	docker rm spring-boot-jdk
 	docker rmi spring-boot-jdk
+}
+
+function rebuild() {
+	clean_image
 	docker build -t spring-boot-jdk -f Dockerfile --build-arg JAR_FILE=$jarfile_reactiverest .
 	docker run -d --name spring-boot-jdk -p 8080:8080 --network complete_dockernet spring-boot-jdk
 }
 
+#update the Dockerfile so it can be rebuild with a new JVM
 function replacer() {
 	var="$@"
 	echo STARTING NEW TEST $var
 	sed -i "1s/.*/$var/" Dockerfile
 }
 
+#get the spring boot start time
 function get_start_time() {
 	docker logs spring-boot-jdk | grep "Started Application"
 }
 
+#single parameter ndicating the outputdir
 function run_test() {
 	echo STARTED AT: `date`
 	sleep 5
@@ -35,11 +46,6 @@ function run_test() {
 	$soapui_bin/loadtestrunner.sh -s"TestSuite 1" -c"TestCase 1" -l"LoadTest 1" -r -f$soapui_outputdir/$1 $soapui_testproject  > /dev/null 2>&1
 	echo COMPLETED AT: `date`
 }
-
-docker stop spring-boot-jdk
-docker rm spring-boot-jdk
-docker rmi spring-boot-jdk
-sleep 60
 
 replacer "FROM oracle\/graalvm-ce:1.0.0-rc9"
 rebuild
