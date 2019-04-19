73ea8e607d9d8e1850fc9297df6970da2453e95f
Maarten Smeets <MaartenSmeets@users.noreply.github.com>
2019-04-19 13:10:02 +0200

    Adding buildscript to create Java 8 and Java 11 JAR files for all frameworks

---
+++ microservicetestcode/buildall.sh
@@ -0,0 +1,43 @@
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+
+cd MicroProfile
+mvn clean package
+cp target/mp-rest-service.jar ../mp-rest-service-8.jar
+cd ..
+
+cd SpringBoot
+mvn clean package
+cp target/sb-rest-service-0.1.0.jar ../sb-rest-service-8.jar
+cd ..
+
+cd SpringBootReactive
+mvn clean package
+cp target/sb-rest-service-reactive-0.1.0.jar ../sb-rest-service-reactive-8.jar
+cd ..
+
+cd SpringFuReactive
+mvn clean package
+cp target/sb-rest-service-reactive-fu-0.1.0.jar ../sb-rest-service-reactive-fu-8.jar
+cd ..
+
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+
+cd MicroProfile
+mvn clean package -Dmaven.compiler.source=11 -Dmaven.compiler.target=11
+cp target/mp-rest-service.jar ../mp-rest-service-11.jar
+cd ..
+
+cd SpringBoot
+mvn clean package -Dmaven.compiler.source=11 -Dmaven.compiler.target=11
+cp target/sb-rest-service-0.1.0.jar ../sb-rest-service-11.jar
+cd ..
+
+cd SpringBootReactive
+mvn clean package -Dmaven.compiler.source=11 -Dmaven.compiler.target=11
+cp target/sb-rest-service-reactive-0.1.0.jar ../sb-rest-service-reactive-11.jar
+cd ..
+
+cd SpringFuReactive
+mvn clean package -Dmaven.compiler.source=11 -Dmaven.compiler.target=11
+cp target/sb-rest-service-reactive-fu-0.1.0.jar ../sb-rest-service-reactive-fu-11.jar
+cd ..
