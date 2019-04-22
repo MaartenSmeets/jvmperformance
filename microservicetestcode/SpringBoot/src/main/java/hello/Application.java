7d2bd9b389041e386f20887d917d1e6ca1dc5b5c
Maarten Smeets <wisp1980@gmail.com>
2019-04-22 12:15:44 +0200

    Checking change

--- microservicetestcode/SpringBoot/src/main/java/hello/Application.java
+++ microservicetestcode/SpringBoot/src/main/java/hello/Application.java
@@ -1,6 +1,7 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
