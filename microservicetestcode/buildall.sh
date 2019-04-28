40825dc014a93996fd14d5c79e69dc8156fff27a
Maarten Smeets <wisp1980@gmail.com>
2019-04-28 14:36:32 +0200

    Updating build script to also do the second Spring Fu variant

--- microservicetestcode/buildall.sh
+++ microservicetestcode/buildall.sh
@@ -1,23 +1,28 @@
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 
 cd MicroProfile
-mvn clean package
+mvn clean package -Dmaven.compiler.source=8 -Dmaven.compiler.target=8
 cp target/mp-rest-service.jar ../mp-rest-service-8.jar
 cd ..
 
 cd SpringBoot
-mvn clean package
+mvn clean package -Dmaven.compiler.source=8 -Dmaven.compiler.target=8
 cp target/sb-rest-service-0.1.0.jar ../sb-rest-service-8.jar
 cd ..
 
 cd SpringBootReactive
-mvn clean package
+mvn clean package -Dmaven.compiler.source=8 -Dmaven.compiler.target=8
 cp target/sb-rest-service-reactive-0.1.0.jar ../sb-rest-service-reactive-8.jar
 cd ..
 
 cd SpringFuReactive
-mvn clean package
+mvn clean package -Dmaven.compiler.source=8 -Dmaven.compiler.target=8
 cp target/sb-rest-service-reactive-fu-0.1.0.jar ../sb-rest-service-reactive-fu-8.jar
+cd ..
+
+cd SpringFuReactive2
+mvn clean package -Dmaven.compiler.source=8 -Dmaven.compiler.target=8
+cp target/sb-rest-service-reactive-fu-0.1.0-jar-with-dependencies.jar ../sb-rest-service-reactive-fu2-8.jar
 cd ..
 
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
@@ -40,4 +45,9 @@ cd ..
 cd SpringFuReactive
 mvn clean package -Dmaven.compiler.source=11 -Dmaven.compiler.target=11
 cp target/sb-rest-service-reactive-fu-0.1.0.jar ../sb-rest-service-reactive-fu-11.jar
+cd ..
+
+cd SpringFuReactive2
+mvn clean package -Dmaven.compiler.source=11 -Dmaven.compiler.target=11
+cp target/sb-rest-service-reactive-fu-0.1.0-jar-with-dependencies.jar ../sb-rest-service-reactive-fu2-11.jar
 cd ..
