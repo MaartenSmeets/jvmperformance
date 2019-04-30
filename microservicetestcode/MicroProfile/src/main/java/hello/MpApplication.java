320cb0fc63f99983e18232c5e055b27f7f596fa2
Maarten Smeets <MaartenSmeets@users.noreply.github.com>
2019-04-30 14:15:00 +0200

    Adding logging

--- microservicetestcode/MicroProfile/src/main/java/hello/MpApplication.java
+++ microservicetestcode/MicroProfile/src/main/java/hello/MpApplication.java
@@ -3,6 +3,7 @@ package hello;
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.lang.management.ManagementFactory;
 
 /**
  * based on https://github.com/OpenLiberty/sample-getting-started
@@ -10,4 +11,10 @@ import javax.ws.rs.core.Application;
 @ApplicationPath("/")
 @ApplicationScoped
 public class MpApplication extends Application {
+     public MpApplication() {
+        super();
+        long currentTime = System.currentTimeMillis();
+        long vmStartTime = ManagementFactory.getRuntimeMXBean().getStartTime();
+        System.out.println("STARTED Application started: "+ (currentTime - vmStartTime));
+    }
 }
