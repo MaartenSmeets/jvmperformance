ab3a499796d9d7156c985eb96282206cc1b4e3d2
Maarten Smeets <MaartenSmeets@users.noreply.github.com>
2019-04-30 13:59:30 +0200

    Adding extra logging

--- microservicetestcode/SpringBootReactive/src/main/java/hello/Application.java
+++ microservicetestcode/SpringBootReactive/src/main/java/hello/Application.java
@@ -1,6 +1,7 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
+import java.lang.management.ManagementFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -8,5 +9,8 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        long currentTime = System.currentTimeMillis();
+        long vmStartTime = ManagementFactory.getRuntimeMXBean().getStartTime();
+        System.out.println("STARTED Application started: "+ (currentTime - vmStartTime));
     }
 }
