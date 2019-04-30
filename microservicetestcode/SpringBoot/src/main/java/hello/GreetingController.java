3842232eff65fde3bc1aa7f0eb95cf82e12e429d
Maarten Smeets <MaartenSmeets@users.noreply.github.com>
2019-04-30 14:34:08 +0200

    Update GreetingController.java

--- microservicetestcode/SpringBoot/src/main/java/hello/GreetingController.java
+++ microservicetestcode/SpringBoot/src/main/java/hello/GreetingController.java
@@ -3,7 +3,7 @@ package hello;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.lang.management.ManagementFactory;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
