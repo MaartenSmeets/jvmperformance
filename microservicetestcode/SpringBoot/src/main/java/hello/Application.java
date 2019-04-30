52d3dd479ec7aef9611065de791d772a4975f35e
Maarten Smeets <MaartenSmeets@users.noreply.github.com>
2019-04-30 13:58:27 +0200

    Adding extra logging for application start time

--- microservicetestcode/SpringBoot/src/main/java/hello/Application.java
+++ microservicetestcode/SpringBoot/src/main/java/hello/Application.java
@@ -1,6 +1,7 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
+import java.lang.management.ManagementFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,8 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        long currentTime = System.currentTimeMillis();
+        long vmStartTime = ManagementFactory.getRuntimeMXBean().getStartTime();
+        System.out.println("STARTED Application started: "+ (currentTime - vmStartTime));
     }
 }
