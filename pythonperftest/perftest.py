00b8e8491d3db161c0b936f071b8b27fdc3e5b0c
Maarten Smeets <MaartenSmeets@users.noreply.github.com>
2019-05-12 20:36:18 +0200

    More robust

--- pythonperftest/perftest.py
+++ pythonperftest/perftest.py
@@ -1,57 +1,71 @@
-import sys
 import os
-import requests
 import signal
+import sys
 import time
-
-URL = "http://localhost:8080/greeting"
-LOGFILEDIR = '.'
+import requests
 
 forks = 4
-if len(sys.argv) == 2:
-    URL = int(sys.argv[1])
-if len(sys.argv) == 3:
-    LOGFILEDIR = int(sys.argv[3])
+URL = os.getenv('URL', "http://localhost:8080/greeting")
+LOGFILEDIR = os.getenv('LOGFILEDIR', "/logs")
+
+try:
+    if not os.path.exists(LOGFILEDIR):
+        os.mkdir(LOGFILEDIR)
+except Exception as e:
+    LOGFILEDIR = os.getcwd()
 
 lines_of_text = []
+pid = 0
+child_pids = []
 
 
 def log(line):
-    lines_of_text.append(line+"\n")
+    lines_of_text.append(line + "\n")
 
-def signal_handler(sig, frame):
-    print("INFO\t{}\tSIGNAL RECEIVED".format(os.getpid())+" "+str(signal.Signals(sig)))
+
+def exit_signal_handler(sig, frame):
+    log("INFO\t{}\tSIGNAL RECEIVED {}".format(os.getpid(), str(signal.Signals(sig))))
+    if pid > 0:
+        for child_pid in child_pids:
+            log("INFO\t{}\tKILLING PID {}".format(os.getpid(), child_pid))
+            try:
+                os.kill(child_pid, signal.SIGINT)
+            except Exception as e:
+                log("CLEANUP_ERROR\t{}\tKILLING PID {} failed: {}".format(os.getpid(), child_pid, str(e)))
     fh.writelines(lines_of_text)
     fh.close()
     sys.exit(0)
 
 
 log("INFO\t{}\tProcess id before forking".format(os.getpid()))
-signal.signal(signal.SIGINT, signal_handler)
-signal.signal(signal.SIGTERM, signal_handler)
+signal.signal(signal.SIGINT, exit_signal_handler)
+signal.signal(signal.SIGTERM, exit_signal_handler)
 
 for i in range(forks):
     try:
         pid = os.fork()
+        if pid > 0:
+            child_pids.append(pid)
     except OSError:
-        log("ERROR\t{}\tCould not create a child process".format(os.getpid()))
+        log("FORK_ERROR\t{}\tCould not create a child process".format(os.getpid()))
         continue
 
     if pid == 0:
-        mypid=os.getpid()
+        mypid = os.getpid()
         log("INFO\t{}\tStarted worker".format(mypid))
         fh = open(LOGFILEDIR + '/' + str(mypid) + '.log', "w")
         while True:
             try:
-                start = time.clock()
+                start = time.process_time()
                 response = requests.get(
                     URL,
                     params={'name': 'Maarten'},
                 )
-                request_time = time.clock() - start
-                log("MEASURE\t{}\t{0:.0f}".format(mypid,request_time))
+                stop = time.process_time()
+                request_time = stop - start
+                log("MEASURE\t{}\t{}".format(mypid, request_time))
             except Exception as e:
-                log("ERROR\t{}\t{}".format(mypid, str(e)))
+                log("REQUEST_ERROR\t{}\t{}".format(mypid, str(e)))
 
 try:
     fh
