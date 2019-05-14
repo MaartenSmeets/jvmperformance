75b2bafa35e9b9af2881b736f1e0c8195a41e1b5
maartensmeets <wisp1980@gmail.com>
2019-05-14 16:19:44 +0200

    Updating NodeJS code to be more similar to Python code to achieve the same

--- nodejsperftest/client.js
+++ nodejsperftest/client.js
@@ -3,55 +3,20 @@ var async = require('async');
 var cluster = require('cluster');
 var process = require('process');
 var pid = process.pid;
-//var numCPUs = require('os').cpus().length / 2;
-var numCPUs = 4;
+var forks = 4;
 var fs = require('fs');
 
 //Example call: node client.js http://9292ov.nl .
 
-var URL = process.argv[2] || "http://localhost:8080/greeting?name=Maarten";
-var LOGFILEDIR = process.argv[3];
-var LOGFILE
+var URL = process.env.URL || 'http://localhost:8080/greeting?name=Maarten';
+var LOGFILEDIR = process.env.LOGFILEDIR || '/logs';
+var LOGFILE;
 
 if (LOGFILEDIR) {
     LOGFILE = String(LOGFILEDIR) + '/' + pid + ".log"
 }
 
 var fileStream;
-var lineBuffer = '';
-var lineBufferCounter = 0;
-var lineBufferSize = 100000000;
-
-function cleanup() {
-    if (LOGFILE) {
-        lineBuffer = lineBuffer + "INFO\t"+pid+"\tCleanup called" + "\n";
-        fileStream.write(lineBuffer);
-        fileStream.end();
-    } else {
-        console.log("INFO\t"+pid+"\tCleanup called");
-    }    
-    cleanup = function() {};
-    return;
-}
-
-process.on('SIGTERM', (error, next) => {
-    cleanup();
-    console.log("INFO\t"+pid+"\tSIGTERM received");
-    process.exit();
-});
-
-process.on('SIGINT', (error, next) => {
-    cleanup();
-    console.log("INFO\t"+pid+"\tSIGINT received");
-    process.exit();
-});
-
-process.on('SIGKILL', (error, next) => {
-    cleanup();
-    console.log("INFO\t"+pid+"\tSIGKILL received");
-    process.exit();
-});
-
 
 if (LOGFILE) {
     fileStream = fs.createWriteStream(LOGFILE, {
@@ -59,51 +24,64 @@ if (LOGFILE) {
     });
 }
 
+var lineBuffer = '';
+
 function mylogger(msg) {
-    if (LOGFILE) {
-        if (lineBufferCounter < lineBufferSize) {
-            lineBuffer = lineBuffer + msg + "\n";
-            lineBufferCounter = lineBufferCounter + 1;
-        } else {
-            fileStream.write(lineBuffer);
-            lineBuffer = '';
-            lineBufferCounter = 0;
-        }
-    } else {
-        console.log(msg);
-    }
+        lineBuffer = lineBuffer + msg + "\n";
 };
+
+function cleanup() {
+    if (LOGFILE) {
+        mylogger("INFO\t"+pid+"\tCleanup called\n");
+        fileStream.write(lineBuffer);
+        fileStream.end();
+        if (cluster.isMaster) {
+            for (const id in cluster.workers) {
+                cluster.workers[id].kill(signal='SIGINT');
+            }
+        }
+    }
+    return;
+}
+
+process.on('SIGTERM', (error, next) => {
+    mylogger("INFO\t"+pid+"\tSIGTERM received");
+    cleanup();
+    process.exit();
+});
+
+process.on('SIGINT', (error, next) => {
+    mylogger("INFO\t"+pid+"\tSIGINT received");
+    cleanup();
+    process.exit();
+});
+
+process.on('SIGKILL', (error, next) => {
+    mylogger("INFO\t"+pid+"\tSIGKILL received");
+    cleanup();
+    process.exit();
+});
 
 if (cluster.isMaster) {
     mylogger("MASTER\t" + pid);    
     mylogger("URL\t" + pid+"\t"+String(URL));
     mylogger("LOGFILE\t" +pid+"\t"+String(LOGFILE));
-    for (var i = 0; i < numCPUs; i++) {
+    for (var i = 0; i < forks; i++) {
         mylogger("INFO\t" + pid + "\tMaster: creating fork: " + i);
-        worker = cluster.fork();
+        cluster.fork();
     };
-
-    cluster.on('exit', (worker, code, signal) => {
-        mylogger("INFO\t" + pid + "\tWorker dies");
-    });
-
-    cluster.on('online', (worker, code, signal) => {
-        mylogger("INFO\t" + pid + "\tWorker started");
-    });
 } else {
     mylogger("WORKER\t" + pid);        
     mylogger("URL\t" + pid+"\t"+String(URL));
     mylogger("LOGFILE\t" + pid+"\t"+String(LOGFILE));
     async.forever(
         function(next) {
-            var startTimeHR = process.hrtime();
             request.get({
-                url: URL
+                url: URL,
+                time: true
             }, function(error, response, body) {
                 if (!error && response.statusCode == 200) {
-                    var durationHR = process.hrtime(startTimeHR);
-                    //mylogger(pid+"\tRequest end. Duration in ms:\t%d", (durationHR[0]*1000)+(durationHR[1] / 1000000));
-                    mylogger("MEASURE\t" + pid+"\t"+String((durationHR[0] * 1000) + (durationHR[1] / 1000000)));
+                    mylogger("MEASURE\t" + pid+"\t"+String(response.elapsedTime));
                     next();
                 } else {
                     if (error && response) {
