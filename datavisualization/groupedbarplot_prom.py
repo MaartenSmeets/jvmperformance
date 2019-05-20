04a3e7f7b01890201a49b2e7b0e309006fa8d8dd
maartensmeets <wisp1980@gmail.com>
2019-05-20 20:47:22 +0200

    Adding Prometheus metrics

--- datavisualization/groupedbarplot_prom.py
+++ datavisualization/groupedbarplot_prom.py
@@ -13,40 +13,40 @@ barWidth = 0.15
 #point the below line to the test output directory
 processdir=sys.argv[1]
 
-averagecmd='cat '+processdir+'/outputfile.txt | grep AVERAGE_PROC | awk \'{print $1","$3}\' > '+processdir+'/average.txt'
-stddevcmd='cat '+processdir+'/outputfile.txt | grep STANDARD_DEVIATION_MS | awk \'{print $1","$3}\' > '+processdir+'/stddev.txt'
+averagecmd='cat '+processdir+'/outputfile.txt | grep PROM_AVERAGE_MS | awk \'{print $1","$3}\' > '+processdir+'/average.txt'
+#stddevcmd='cat '+processdir+'/outputfile.txt | grep STANDARD_DEVIATION_MS | awk \'{print $1","$3}\' > '+processdir+'/stddev.txt'
 print ('Executing: '+averagecmd)
 os.system(averagecmd)
 
-print ('Executing: '+stddevcmd)
-os.system(stddevcmd)
+#print ('Executing: '+stddevcmd)
+#os.system(stddevcmd)
 
 df1 = pd.read_csv(processdir+'/average.txt', sep=',', header=None)
-df2 = pd.read_csv(processdir+'/stddev.txt', sep=',', header=None)
+#df2 = pd.read_csv(processdir+'/stddev.txt', sep=',', header=None)
 df1.columns = ['jvm_framework_ident','average']
-df2.columns = ['jvm_framework_ident','stddev']
+#df2.columns = ['jvm_framework_ident','stddev']
 
 
-df1 = pd.merge(df1,df2,on="jvm_framework_ident")
+#df1 = pd.merge(df1,df2,on="jvm_framework_ident")
 
 df1[['jvm','framework']] = df1['jvm_framework_ident'].str.split('_',expand=True)
-df2[['jvm','framework']] = df2['jvm_framework_ident'].str.split('_',expand=True)
+#df2[['jvm','framework']] = df2['jvm_framework_ident'].str.split('_',expand=True)
 
-#df1 = df1[df1.jvm != 'adoptopenjdkshenandoahgc']
-#df2 = df2[df2.jvm != 'adoptopenjdkshenandoahgc']
-#df1 = df1[df1.jvm != 'openj9metronome']
+df1 = df1[df1.jvm != 'openj9metronome']
+df1 = df1[df1.jvm != 'adoptopenjdkshenandoahgc']
+#df1 = df1[df1.framework != 'mp']
 #df2 = df2[df2.jvm != 'openj9metronome']
 
 #check data
 jvms=df1.jvm.unique()
 jvms.sort()
-jvms2=df2.jvm.unique()
-jvms2.sort()
+#jvms2=df2.jvm.unique()
+#jvms2.sort()
 frameworks=df1.framework.unique()
 frameworks.sort()
-if len(list(set(jvms) & set(jvms2)))!=len(jvms):
-    print ('Averages and Standard Errors do not contain data for the same JVMs'+str(list(set(jvms) & set(jvms2))))
-    exit(1)
+#if len(list(set(jvms) & set(jvms2)))!=len(jvms):
+#    print ('Averages and Standard Errors do not contain data for the same JVMs'+str(list(set(jvms) & set(jvms2))))
+#    exit(1)
 
 framework_dict={'mp':'Microprofile','sb':'Spring Boot','sbreactive':'WebFlux','sbfu':'Spring Fu','vertx':'Vert.x','akka':'Akka'}
 jvm_dict={'zing':'Azul Zing','corretto':'Amazon Corretto','graalvm':'GraalVM','openj9':'OpenJ9','adoptopenjdk':'AdoptOpenJDK','oraclejdk':'Oracle JDK','zuluopenjdk':'Azul Zulu','openjdk':'OpenJDK','adoptopenjdkdd':'AdoptOpenJDK\nDocker <- Docker','adoptopenjdkdl':'AdoptOpenJDK\nDocker <- Local','adoptopenjdkll':'AdoptOpenJDK\nLocal <- Local','adoptopenjdkld':'AdoptOpenJDK\nLocal <- Docker','adoptopenjdkserial':'OpenJDK\nSerial','adoptopenjdkcms':'OpenJDK\nCMS','adoptopenjdkpargc': 'OpenJDK\nParallel','adoptopenjdkg1gc':'OpenJDK\nG1GC','openj9gencon':'OpenJ9\nGencon','openj9balanced':'OpenJ9\nBalanced','openj9metronome':'OpenJ9\nMetronome','openj9optavgpause':'OpenJ9\nOptAvgPause','openj9optthrupu':'OpenJ9\nOptThruPu','adoptopenjdkshenandoahgc':'OpenJDK12\nShenandoah','adoptopenjdkzgc':'OpenJDK12\nZGC'}
@@ -57,11 +57,11 @@ df1['framework_descr'] = df1['framework'].map(framework_dict)
 df1.sort_values(['jvm', 'framework'], ascending=[True, True])
 print (df1)
 #check data
-for jvm in jvms:
-    averages=df1.loc[df1['jvm'] == jvm, 'average']
-    if (len(averages) < len(frameworks)):
-        print ('Dataset for '+jvm+' incomplete! Found '+str(len(averages))+' averaged but expected '+str(len(frameworks)))
-        exit(1)
+#for jvm in jvms:
+#    averages=df1.loc[df1['jvm'] == jvm, 'average']
+#    if (len(averages) < len(frameworks)):
+#        print ('Dataset for '+jvm+' incomplete! Found '+str(len(averages))+' averaged but expected '+str(len(frameworks)))
+#        exit(1)
 
 #based on https://python-graph-gallery.com/11-grouped-barplot/
 #calculate bar location. rowloc[0] is the location for the first bar in every group (group=keys from framework_dict)
@@ -75,10 +75,10 @@ averages=[]
 for jvm in jvms:
     averages.append(df1.loc[df1['jvm']==jvm,'average'])
 
-stddevs=[]
+#stddevs=[]
 #Add for each JVM averages (every average is for a specific framework)
-for jvm in jvms:
-    stddevs.append(df1.loc[df1['jvm']==jvm,'stddev'])
+#for jvm in jvms:
+#    stddevs.append(df1.loc[df1['jvm']==jvm,'stddev'])
 
 # Make the plot
 figure(num=None, figsize=(16, 6))
@@ -90,13 +90,13 @@ if (len(frameworks)==1):
     plt.xticks([rowloc[x] for x in np.arange(0,len(jvms))], [jvm_dict[x] for x in jvms])
 else:
     plt.xticks(rowloc[int(len(rowloc)/2)], [framework_dict[x] for x in frameworks])
-    plt.legend([jvm_dict[x] for x in jvms],loc=2)
+    plt.legend([jvm_dict[x] for x in jvms])
     plt.xlabel('Framework')
 
-plt.ylim(1.2, 1.8)
+#plt.ylim(1.2, 1.8)
 
 plt.ylabel('Average response time [ms]')
 
-plt.title('Average response time per GC algorithm')
+plt.title('Average response time Prometheus')
 plt.tight_layout()
 plt.savefig(sys.argv[2], dpi=100)
