2638317638507bb1650699f9a6dc3e266b845d29
maartensmeets <wisp1980@gmail.com>
2019-09-14 19:09:51 +0200

    Updating logger

--- testscripts_v2/run_test.py
+++ testscripts_v2/run_test.py
@@ -18,15 +18,18 @@ logger.setLevel(logging.DEBUG)
 # create console handler and set level to debug
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
-
+fh = logging.FileHandler('output.log')
+fh.setLevel(logging.DEBUG)
 # create formatter
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 # add formatter to ch
 ch.setFormatter(formatter)
+fh.setFormatter(formatter)
 
 # add ch to logger
 logger.addHandler(ch)
+logger.addHandler(fh)
 
 # GC algorithms
 gc_openj9 = [
